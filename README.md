356182358f5a8f80a3d13495233ebe0f45982e45
Hiro <84511935+Hiro420@users.noreply.github.com>
2024-04-02 03:28:17 +0300

    upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Discord-Lootbox-Opener
-Automatic opener for 2024 April Fools discord lootboxes
+Automatic opener for 2024 April Fools discord lootboxes.
+Made 100% for fun, not useful in any way.
+
+# Usage
+1- Copy Authorization, Cookie and X-Super-Properties from a previous request
+2- Go to postData, headers, and set them there
+3- `npm i`
+4- `npm run start`
+
---
+++ openLootbox.ts
@@ -0,0 +1,94 @@
+import axios from 'axios';
+
+export const colorize = new (class {
+    color = (code: number, ended = false, ...messages: any[]) =>
+    `\x1b[${code}m${messages.join(" ")}${ended ? "\x1b[0m" : ""}${ended ? "" : "\x1b[0m"}`;
+    black = this.color.bind(null, 30, false);
+    red = this.color.bind(null, 31, false);
+    green = this.color.bind(null, 32, false);
+    yellow = this.color.bind(this, 33, false);
+    blue = this.color.bind(this, 34, false);
+    magenta = this.color.bind(this, 35, false);
+    cyan = this.color.bind(this, 36, false);
+    white = this.color.bind(this, 37, false);
+    bgBlack = this.color.bind(this, 40, true);
+    bgRed = this.color.bind(this, 41, true);
+    bgGreen = this.color.bind(this, 42, true);
+    bgYellow = this.color.bind(this, 43, true);
+    bgBlue = this.color.bind(this, 44, true);
+    bgMagenta = this.color.bind(this, 45, true);
+    bgCyan = this.color.bind(this, 46, true);
+    bgWhite = this.color.bind(this, 47, true);
+})();
+
+const itemMap: any = {
+    '1214340999644446720': "Buster Blade",
+    '1214340999644446721': "Cute Plushie",
+    '1214340999644446722': "Wump Shell",
+    '1214340999644446723': "Speed Boost",
+    '1214340999644446724': "⮕⬆⬇⮕⬆⬇",
+    '1214340999644446725': "Power Helmet",
+    '1214340999644446726': "Quack!!",
+    '1214340999644446727': "OHHHHH BANANA",
+    '1214340999644446728': "Dream Hammer"
+}
+
+async function postData() {
+    const url = 'https://canary.discord.com/api/v9/users/@me/lootboxes/open';
+    const headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,he;q=0.9',
+        'Authorization': '', // here you put your token
+        'Content-Length': '0',
+        'Cookie': '', // here you put your cookie
+        'Origin': 'https://canary.discord.com',
+        'Referer': 'https://canary.discord.com/channels/@me',
+        'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.207 Chrome/120.0.6099.291 Electron/28.2.7 Safari/537.36',
+        'X-Debug-Options': 'bugReporterEnabled',
+        'X-Discord-Locale': 'en-US',
+        'X-Discord-Timezone': 'Asia/Jerusalem',
+        'X-Super-Properties': '' // here you put your X-Super-Properties from previous requests
+    };
+
+    const response = await axios.post(url, {}, { headers: headers });
+    return response.data;
+}
+
+let intervalCount = 0;
+
+async function mainFunction() {
+    const startTime = Date.now(); 
+    try {
+        const data = await postData();
+        console.clear();
+        const item = data.opened_item;
+        intervalCount++;
+        console.log('Opened:', colorize.green(itemMap[item]));
+        console.log('Items Count:');
+        const itemIds = Object.keys(data.user_lootbox_data.opened_items).sort();
+        for (const itemId of itemIds) {
+            const itemName = itemMap[itemId];
+            if (itemName) {
+                console.log(`${colorize.magenta(itemName)} - ${colorize.green(data.user_lootbox_data.opened_items[itemId])}`);
+            }
+        }
+    } catch (error) {
+        console.log('Error:', error);
+        setTimeout(mainFunction, 200);
+        return;
+    } finally {
+        const endTime = Date.now();
+        const elapsedTime = endTime - startTime;
+        console.log(`Opened ${intervalCount}th box in ${elapsedTime}ms`);
+        setTimeout(mainFunction, 2100 + elapsedTime);
+    }
+}
+
+mainFunction();
